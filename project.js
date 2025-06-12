bbbec3dd1a82011c4fefd57b52def7f805bbecfe
Pratiksh Saini <pratikshsaini10@gmail.com>
2025-06-12 13:46:22 +0530

    Third Feature Completed

--- project.js
+++ project.js
@@ -24,5 +24,18 @@ const getNumberOfLines = () => {
   }
 };
 
-const depositamount = deposit();
+const getBet = (balance, lines) => {
+  while (true) {
+    const bet = prompt("Enter the bet per line: ");
+    const numberBet = parseFloat(bet);
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+      console.log("Invalid bet, try again.");
+    } else {
+      return numberBet;
+    }
+  }
+};
+
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
